2a5b70e92d8e0ca0903f9fe9b82c83a79fc06776
SadeEtensh <eteneshsade550305@gmail.com>
2023-10-02 11:15:02 +0300

    Flexbox completed Card Section

--- components/FlatCard.tsx
+++ components/FlatCard.tsx
@@ -4,9 +4,52 @@ import React from 'react';
 export default function FlatCard() {
   return (
     <View>
-      <Text>FlatCard</Text>
+      <Text style={styles.headingText}>Flat Card</Text>
+      <View style={styles.Container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Green</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+    paddingTop: 10,
+  },
+  Container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+  },
+  cardOne: {
+    backgroundColor: '#EF5354',
+  },
+  cardTwo: {
+    backgroundColor: '#50DBB4',
+  },
+  cardThree: {
+    backgroundColor: '#5DA3FA',
+  },
+});
