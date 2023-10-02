ca6ee6d478e4035145cb97a2d1441ac9e6b9ede2
SadeEtensh <eteneshsade550305@gmail.com>
2023-10-02 10:45:37 +0300

    File organization

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
+import FlatCard from './components/FlatCard';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>App</Text>
+        <FlatCard />
       </ScrollView>
     </SafeAreaView>
   );
