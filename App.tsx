f205a2dae44d0d2feded782767897daf072df0d6
SadeEtensh <eteneshsade550305@gmail.com>
2023-10-02 10:39:22 +0300

    updated App.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,14 @@
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
 
-export default function App(): JSX.Element {
+const App = () => {
   return (
-    <View>
-      <Text>Hello word</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <Text>App</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+export default App;
